9ab1cd357b7461d3b9c95b9a8fe2ff31f20f1012
Giovanni Bertoncelli <bertonc96@gmail.com>
2025-03-30 11:38:06 +0000

    feat(bot): create bot in lib

--- plugins/bot.js
+++ plugins/bot.js
@@ -1,14 +1,10 @@
 import { hydrateReply } from '@grammyjs/parse-mode';
 import fp from 'fastify-plugin';
 import { Bot, webhookCallback } from 'grammy';
+import { createBot } from '../lib/bot';
 
 async function plugin(fastify, opts) {
-  const token = process.env['TELEGRAM_BOT_SECRET']
-  const secret = process.env['TELEGRAM_API_SECRET']
-  const bot = new Bot(token);
-
-  bot.use(hydrateReply);
-
+  const bot = await createBot();
   fastify.post(`/${token}`, webhookCallback(bot, 'fastify', {
     secretToken: secret
   }));
