52f75ea32e8db7cb8d2f56adb76ebe8c4c7967b0
Giovanni Bertoncelli <bertonc96@gmail.com>
2025-04-02 17:13:14 +0000

    fix: install

--- install/install.sh
+++ install/install.sh
@@ -1,11 +1,14 @@
 apt-get install nodejs
 apt-get install npm
 curl -fsSL https://get.pnpm.io/install.sh | sh -
-sudo systemctl daemon-reload
-sudo systemctl enable spamiabot.service
-sudo systemctl start spamiabot.service
-sudo systemctl status spamiabot.service
-apt-get install nginx
+
 cp ./spamiabot.service /etc/systemd/system/
+sudo systemctl enable spamiabot.service
+
+apt-get install nginx
 cp ./default /etc/nginx/sites-available/default
+sudo systemctl daemon-reload
+
 systemctl restart nginx
+systemctl restart spamiabot
+sudo systemctl status spamiabot.service
