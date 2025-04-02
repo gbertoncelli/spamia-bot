0fb945aae1562fcf5c869ebde5be2ac2fff825a5
Giovanni Bertoncelli <bertonc96@gmail.com>
2025-04-02 17:04:40 +0000

    fix: nginx

--- install/install.sh
+++ install/install.sh
@@ -1,4 +1,5 @@
 cp ./spamiabot.service /etc/systemd/system/
+cp ./default /etc/nginx/sites-available/default
 sudo systemctl daemon-reload
 sudo systemctl enable spamiabot.service
 sudo systemctl start spamiabot.service
